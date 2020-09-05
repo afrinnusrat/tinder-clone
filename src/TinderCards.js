da82014131452c120e03938932029c18e4519354
wildenali <wildenwildenaliali@gmail.com>
2020-09-05 23:01:55 +0700

    Membuat TinderCard dan dengan dummy contoh nya

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import TinderCard from "react-tinder-card";
 
 function TinderCards() {
+  const [people, setPeople] = useState([
+    {
+      name: "The Professor",
+      url:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Premios_Goya_2020_-_%C3%81lvaro_Morte_%28Cropped%29.jpg/1200px-Premios_Goya_2020_-_%C3%81lvaro_Morte_%28Cropped%29.jpg",
+    },
+    {
+      name: "Denver",
+      url:
+        "https://pley.today/__export/1585133520847/sites/mui/img/2020/03/25/jaime-lorente-la-casa-de-papel-kzxc--1248x698xabc.jpg_1276763261.jpg",
+    },
+  ]);
+
+  // setPeople([...people, "wilden", "ali"]); // ini contoh cara pake useState
+
   return (
     <div>
       <h1>Ini Tinder Cards</h1>
+      {people.map((person) => (
+        <TinderCard className="swipe" key={person.name}>
+          <div
+            style={{ backgroundImage: `url(${person.url})` }}
+            className="card"
+          >
+            <h3>{person.name}</h3>
+          </div>
+        </TinderCard>
+      ))}
     </div>
   );
 }
