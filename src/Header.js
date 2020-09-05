6e56c8fbcd56f52b85f65b25d1e42925193ce9b9
wildenali <wildenwildenaliali@gmail.com>
2020-09-05 21:02:34 +0700

    sedikit styling untuk header

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
+import "./Header.css";
 import PersonIcon from "@material-ui/icons/Person";
+import ForumIcon from "@material-ui/icons/Forum";
 
 function Header() {
   return (
-    <div>
+    <div className="header">
       <PersonIcon />
       <h1>Header</h1>
+      <ForumIcon />
     </div>
   );
 }
