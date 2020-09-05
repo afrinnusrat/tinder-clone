888af51b89989badcf1f46b1cfff840dce888c15
wildenali <wildenwildenaliali@gmail.com>
2020-09-05 21:48:10 +0700

    Pasang Router, Switch, dan Route di App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      {/* Header */}
-      <Header />
-
-      {/* Tinder Cards */}
-      {/* Button below tinder cards */}
-
-      {/* Chars screen */}
-      {/* Individual chat screen */}
+      <Router>
+        {/* Header */}
+        <Header />
+        <Switch>
+          <Route path="/chat">
+            <h1>I am Chat page</h1>
+          </Route>
+          <Route path="/">
+            <h1>I am Home page</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+{
+  /* Tinder Cards */
+}
+{
+  /* Button below tinder cards */
+}
+{
+  /* Chars screen */
+}
+{
+  /* Individual chat screen */
+}
