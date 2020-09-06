13e4d2fdff042d9c662391e25b560203a31741e4
wildenali <wildenwildenaliali@gmail.com>
2020-09-06 17:30:07 +0700

    Membuat komponen chat

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./Header";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import TinderCards from "./TinderCards";
 import SwipeButtons from "./SwipeButtons";
+import Chats from "./Chats";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Switch>
           <Route path="/chat">
             <Header backButton="/" />
-            <h1>I am Chat page</h1>
+            <Chats />
           </Route>
           <Route path="/">
             <Header />
