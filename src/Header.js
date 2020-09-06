af72cb362cd827e395aeddc654987c381f8d15ae
wildenali <wildenwildenaliali@gmail.com>
2020-09-06 17:04:43 +0700

    ketika di click button '<' di chat page, dia kembali ke home page

--- src/Header.js
+++ src/Header.js
@@ -4,14 +4,16 @@ import PersonIcon from "@material-ui/icons/Person";
 import ForumIcon from "@material-ui/icons/Forum";
 import IconButton from "@material-ui/core/IconButton";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 function Header({ backButton }) {
+  const history = useHistory();
+
   return (
     <div className="header">
       {backButton ? (
-        <IconButton>
-          <ArrowBackIosIcon fontSize="large" className="header_iconBack" />
+        <IconButton onClick={() => history.replace(backButton)}>
+          <ArrowBackIosIcon fontSize="large" className="header_icon" />
         </IconButton>
       ) : (
         <IconButton>
